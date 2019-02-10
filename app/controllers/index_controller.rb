03533c007346811ca46514e5800a8d06439a98b6
chatanisota <newpc1271@hotmail.co.jp>
2019-02-10 23:32:29 +0900

    [develop] add ; test code

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,2 +1,19 @@
 class IndexController < ApplicationController
+
+  def index
+    if request.post? then
+      @title = "RESULT"
+      @main_text = "you typed : " + params['name']
+      @value = params['name']
+    else
+      @title = "Index"
+      @main_text = "type text, please"
+      @value = ''
+    end
+  end
+
+  def chatani
+    redirect_to action: :index, params: {'name': 'chatani sota'}
+  end
+
 end
