a9f32c8150cb41e1127d734248099bf6cb32bb27
kassyinkyoto11 <kassyinkyoto11@gmail.com>
2019-02-12 16:01:24 +0900

    [master] join kassyinkyoto

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'index/index'
+  post "index/index"
+  
+  get 'index', to: 'index#index'
+  get 'index/chatani'
+end
