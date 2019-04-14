f9532a73712e39d1f7b12a8cd48a34ef4a4323b7
chatanisota <newpc1271@hotmail.co.jp>
2019-04-14 16:24:38 +0900

    [develop] fix : login and signup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,24 @@
 class ApplicationController < ActionController::Base
+
+  before_action :set_current_account_id
   before_action :set_current_user
-  #
-  def set_current_user
+
+  def set_current_account_id
     if current_account then
       @current_account_id = current_account.id
+    end
+  end
+
+  #
+  def set_current_user
+    if @current_account_id then
       @current_user = User.find_by(account_id: @current_account_id)
+    end
+  end
+
+  def check_current_user
+    if !@current_user then
+      redirect_to("/signup3")
     end
   end
   #
